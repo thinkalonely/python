9d6108c6136e3d30873199a1a28b3e73398b3385
thinkalonely <thinkalonely@gmail.com>
2017-09-30 10:24:02 +0800

    a decoretor

---
+++ logger.py
@@ -0,0 +1,16 @@
+import functools
+
+def logger(text):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kw):
+            print('%s %s():' % (text, func.__name__))
+            return func(*args, **kw)
+        return wrapper
+    return decorator
+
+@logger('DEBUG')
+def today():
+    print('2015-3-25')
+
+today()
