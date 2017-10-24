222c9a65998caa0bf880336043dbad4b0a2bb795
thinkalonely <thinkalonely@gmail.com>
2017-10-24 18:02:53 +0800

    updated calc_md5

--- calc_md5.py
+++ calc_md5.py
@@ -5,20 +5,23 @@ import hashlib
 
 db = {}
 salt = 'pythonisbest'
-print('请注册：')
-username = input('Please input your username: ')
-if username in db:
-    print(username + 'is already registed!')
-password = input('Please input your password: ')
 
 def get_md5(password):
     pw_md5 = hashlib.md5()
     pw_md5.update(password.encode('utf-8'))
     return pw_md5.hexdigest()
 
-def register(user, passwd):
-    db[user] = get_md5(user + passwd + salt)
-    print('注册成功，请登录：')
+def register():
+    print('请注册：')
+    user = input('Please input your username: ')
+    if user in db:
+        print(user + 'is already registed!')
+        print('请登录：')
+    else:
+        passwd = input('Please input your password: ')
+        db[user] = get_md5(user + passwd + salt)
+        print('注册成功，请登录：')
+    login()
 
 def login():
     user = input('Please input user: ')
@@ -30,8 +33,10 @@ def login():
             print('登录成功！')
         else:
             print('密码错误！')
+            print('重新登录')
+            login()
     else:
         print('User not exist!')
-
-register(username, password)
-login()
+        register()
+register()
+print(db)
