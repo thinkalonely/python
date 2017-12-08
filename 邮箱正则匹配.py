3ddd395b148bef7cc7b27e974e7eb811e44c2012
thinkalonely <thinkalonely@gmail.com>
2017-12-08 16:29:00 +0800

    updated 邮箱的正则

--- 邮箱正则匹配.py
+++ 邮箱正则匹配.py
@@ -3,10 +3,13 @@
 
 import re
 
-mail = 'test.3@test.com'
+mail = 'adads@dadsa-4.org'
 rechar = r'^([a-z_0-9.-]{1,64})@([a-z0-9-]{1,200})(.{1,5}[a-z]{1,6})$'
 # 用户名匹配字母或数字或-或_或.的组合
 # 域名匹配 字母数字‘-’的组合
 
 result = re.match(rechar, mail)
 print(result)
+print('用户名:', result.group(1))
+print('域  名:', result.group(2))
+print('后  缀:', result.group(3))
