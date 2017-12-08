4a1f5291005ec2632341fb185403c73e4f00bc17
thinkalonely <thinkalonely@gmail.com>
2017-12-08 16:22:30 +0800

    updated 邮箱的正则

--- 邮箱正则匹配.py
+++ 邮箱正则匹配.py
@@ -5,6 +5,8 @@ import re
 
 mail = 'test.3@test.com'
 rechar = r'^([a-z_0-9.-]{1,64})@([a-z0-9-]{1,200})(.{1,5}[a-z]{1,6})$'
+# 用户名匹配字母或数字或-或_或.的组合
+# 域名匹配 字母数字‘-’的组合
 
 result = re.match(rechar, mail)
 print(result)
