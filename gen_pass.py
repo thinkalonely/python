e20388724ef7fd0314e5423cb85410d77e9bfbff
thinkalonely <thinkalonely@gmail.com>
2018-01-30 15:00:09 +0800

    生成8位随机大小写加数字的字符串

--- gen_pass.py
+++ gen_pass.py
@@ -1,12 +1,7 @@
 import random
 import string
-#list1 = ['z', 'y','x','w','v','u','t','s','r','q','p','o','n','m','l','k','j','i','h','g','f','e','d','c','b','a']
-#list2 = [i for i in range(1, 10)]
 
-#st = [i for i in random.sample(list1, 2)]
-#num = [i for i in random.sample(list2, 6)]
 def gen_pass(size=8, chars=string.ascii_letters + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
-#print(st+num)
-pass1 = gen_pass()
-print(pass1)
+
+gen_pass()
