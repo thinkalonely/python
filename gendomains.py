de665aaa343f85c78998cedc11a87a8871dfb554
thinkalonely <thinkalonely@gmail.com>
2018-02-06 11:45:38 +0800

    生成随机大小写加数字的字符串

--- gendomains.py
+++ gendomains.py
@@ -1,20 +1,20 @@
 import random
-
+import string
 order = input("Please input:\n 字母加数字选择 1：\n 全字母选择2：\n 全数字选择3：\n 清空文本按 d: ")
 
 tmplist = []
-list1 = ['z','y','x','w','v','u','t','s','r','q','p','o','n','m','l','k','j','i','h','g','f','e','d','c','b','a','1','2','3','5','6','7','8','9']
-list2 = ['z','y','x','w','v','u','t','s','r','q','p','o','n','m','l','k','j','i','h','g','f','e','d','c','b','a']
+list1 = string.ascii_lowercase + string.digits
+list2 = string.ascii_lowercase
 
-numlist = [str(x) for x in range(0, 10)]
+numlist = string.digits
 namepath ='C:\\users\\test\desktop\\domains.txt'
 
 if order == '1':
-	tmplist = list1
+    tmplist = list1
 elif order == '2':
-	tmplist = list2
+    tmplist = list2
 elif order == '3':
-	tmplist = numlist
+    tmplist = numlist
 elif order == 'd':
 	print('正在清除数据...')
 	with open(namepath, 'w') as f:
