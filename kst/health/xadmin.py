c87102154fdcb716eaf97e3a32c266d2167dcd71
thinkalonely <thinkalonely@gmail.com>
2018-05-11 13:52:24 +0800

    use xadmin

---
+++ kst/health/xadmin.py
@@ -0,0 +1,16 @@
+import xadmin
+from django.contrib import admin
+from .models import Article, Industry
+
+# class ArticleAdmin(admin.ModelAdmin):
+class ArticleAdmin(object):
+
+    list_display = ('title', 'id', 'keywords', 'pub_date')
+
+# class IndustryAdmin(admin.ModelAdmin):
+class IndustryAdmin(object):
+    list_display = ('title', 'id', 'keywords', 'pub_date')
+    # filter(list_display, sorted('pub_date'))
+
+xadmin.site.register(Article, ArticleAdmin)
+xadmin.site.register(Industry, IndustryAdmin)
