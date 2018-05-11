b504cce98e87698e324a701452a13cd8f2f7c428
thinkalonely <thinkalonely@gmail.com>
2018-05-11 11:08:39 +0800

    update admin

--- kst/health/admin.py
+++ kst/health/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Article, Contact, Provide, Service, People, Advantage, Industry
+from .models import Article, Industry
 
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'id', 'keywords', 'pub_date')
@@ -7,27 +7,6 @@ class ArticleAdmin(admin.ModelAdmin):
 class IndustryAdmin(admin.ModelAdmin):
     list_display = ('title', 'id', 'keywords', 'pub_date')
 
-class ContactAdmin(admin.ModelAdmin):
-    list_display = ('name', 'contact_phone', 'contact_email', 'zip_code', 'contact_address')
-
-class ProvideAdmin(admin.ModelAdmin):
-    list_display = ('provide_title', 'provide_text')
-
-class ServiceAdmin(admin.ModelAdmin):
-    list_display = ('title', 'text')
-
-class PeopleAdmin(admin.ModelAdmin):
-    list_display = ('id', 'people')
-    ordering = ('id',)
-
-class AdvantageAdmin(admin.ModelAdmin):
-    list_display = ('id', 'advantage')
-    ordering = ('id',)
 
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(Industry, IndustryAdmin)
-admin.site.register(Contact, ContactAdmin)
-admin.site.register(Provide, ProvideAdmin)
-admin.site.register(Service, ServiceAdmin)
-admin.site.register(People, PeopleAdmin)
-admin.site.register(Advantage, AdvantageAdmin)
